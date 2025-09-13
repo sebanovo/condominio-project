24b5ab6116791c4813bdbc15d65c698d9216da82
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2025-09-13 17:22:24 -0400

    add models in admin site

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import (
+    Casa,
+    Perfil,
+    Vehiculo,
+    AreaComun,
+    Reserva,
+    Multa,
+    IngresoSalida,
+    Extranjero,
+)
 
 # Register your models here.
+
+admin.site.register(Casa)
+admin.site.register(Perfil)
+admin.site.register(Vehiculo)
+admin.site.register(AreaComun)
+admin.site.register(Reserva)
+admin.site.register(Multa)
+admin.site.register(IngresoSalida)
+admin.site.register(Extranjero)
